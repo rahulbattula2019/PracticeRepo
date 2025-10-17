08de4c95a107260d11b283eb5f94b9d6a5fe8561
rahul <103805829+rahulbattula2019@users.noreply.github.com>
2025-10-17 06:46:20 +0000

    vamsi code

--- practice.py
+++ practice.py
@@ -1,3 +1,37 @@
-size = int(input("Enter the list size: "))
-nums = [int(input()) for i in range(size)]
-print(nums)
+import sys
+sys.setrecursionlimit(200000) 
+ 
+def solution(T):
+    N = len(T)
+    
+    adj = [[] for _ in range(N)]
+    
+    for I in range(N):
+        P = T[I]
+        if P != I:
+            adj[I].append(P)
+            adj[P].append(I)
+ 
+    def dfs(u, parent, has_ticket):
+        
+        max_extension_length = 0
+ 
+        for v in adj[u]:
+            if v != parent:
+                v_is_odd = (v % 2 != 0)
+                
+                if not v_is_odd:
+                    new_has_ticket = has_ticket
+                    path_len_v = dfs(v, u, new_has_ticket)
+                    max_extension_length = max(max_extension_length, path_len_v)
+ 
+                else: 
+                    if has_ticket:
+                        new_has_ticket = False
+                        path_len_v = dfs(v, u, new_has_ticket)
+                        max_extension_length = max(max_extension_length, path_len_v)
+                        
+        return 1 + max_extension_length
+ 
+    return dfs(0, -1, True)
+ 
